d11c18ec82e4299f2ba70c895421ffaa25ad9cd1
sarival <sarival@sarival.com.br>
2020-05-21 06:00:32 -0300

    buscando dados do paciente e calc do imc

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,15 @@
-			// retorna h1 
-			let titulo = document.querySelector('.titulo');
-			titulo.textContent = "Aparecida Nutricionista";
+// retorna h1 
+let titulo = document.querySelector('.titulo');
+titulo.textContent = "Aparecida Nutricionista";
+
+//buscando dados do primeiro paciente
+let paciente = document.querySelector("#primeiro-paciente");
+let peso = paciente.querySelector(".info-peso").textContent;
+let altura = paciente.querySelector(".info-altura").textContent;
+
+// calculo do imc
+let imc = peso / (altura * altura);
+
+console.log(imc);
+
+
