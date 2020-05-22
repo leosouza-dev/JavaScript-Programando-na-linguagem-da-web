fe6fb01eec5122bd804b9220a50056fcc9551f40
sarival <sarival@sarival.com.br>
2020-05-22 16:02:54 -0300

    loop for

--- js/principal.js
+++ js/principal.js
@@ -3,27 +3,60 @@ let titulo = document.querySelector('.titulo');
 titulo.textContent = "Aparecida Nutricionista";
 
 //buscando dados do primeiro paciente
-let paciente = document.querySelector("#primeiro-paciente");
-let peso = paciente.querySelector(".info-peso").textContent;
-let altura = paciente.querySelector(".info-altura").textContent;
-let imcTd = paciente.querySelector(".info-imc");
+let pacientes = document.querySelectorAll(".paciente");
 
-// operadores lógico - validação
-let pesoInvalido = (peso <= 0) || (peso >= 1000);
-let alturaInvalido = (altura <= 0) || (altura >= 3);
+for (let i = 0; i < pacientes.length; i++) {
+  const paciente = pacientes[i];
 
-if(pesoInvalido){
-  imcTd.textContent = "Peso Inválido";
-}
-else{
-  if(alturaInvalido){
-    imcTd.textContent = "Altura Inválida";
+  let peso = paciente.querySelector(".info-peso").textContent;
+  let altura = paciente.querySelector(".info-altura").textContent;
+  let imcTd = paciente.querySelector(".info-imc");
+  
+  // operadores lógico - validação
+  let pesoInvalido = (peso <= 0) || (peso >= 1000);
+  let alturaInvalido = (altura <= 0) || (altura >= 3);
+  
+  if(pesoInvalido){
+    imcTd.textContent = "Peso Inválido";
   }
   else{
-    let imc = peso / (altura * altura);
-    imcTd.textContent = imc;
+    if(alturaInvalido){
+      imcTd.textContent = "Altura Inválida";
+    }
+    else{
+      let imc = peso / (altura * altura);
+      imcTd.textContent = imc.toFixed(2);
+    }
   }
+  
 }
+
+// Outra opção - ForEach
+// pacientes.forEach(paciente => {
+
+//   let peso = paciente.querySelector(".info-peso").textContent;
+//   let altura = paciente.querySelector(".info-altura").textContent;
+//   let imcTd = paciente.querySelector(".info-imc");
+  
+//   // operadores lógico - validação
+//   let pesoInvalido = (peso <= 0) || (peso >= 1000);
+//   let alturaInvalido = (altura <= 0) || (altura >= 3);
+  
+//   if(pesoInvalido){
+//     imcTd.textContent = "Peso Inválido";
+//   }
+//   else{
+//     if(alturaInvalido){
+//       imcTd.textContent = "Altura Inválida";
+//     }
+//     else{
+//       let imc = peso / (altura * altura);
+//       imcTd.textContent = imc;
+//     }
+//   }
+  
+// });
+
 
 
 
