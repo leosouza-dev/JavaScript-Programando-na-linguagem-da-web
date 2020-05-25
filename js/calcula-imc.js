45950ca20b1c54d5f3accd5fedf13561700d2721
sarival <sarival@sarival.com.br>
2020-05-25 16:38:20 -0300

    separando os codigos js

---
+++ js/calcula-imc.js
@@ -0,0 +1,44 @@
+// retorna h1 
+let titulo = document.querySelector('.titulo');
+titulo.textContent = "Aparecida Nutricionista";
+
+//buscando dados dos pacientes
+let pacientes = document.querySelectorAll(".paciente");
+
+for (let i = 0; i < pacientes.length; i++) {
+  const paciente = pacientes[i];
+
+  let peso = paciente.querySelector(".info-peso").textContent;
+  let altura = paciente.querySelector(".info-altura").textContent;
+  let imcTd = paciente.querySelector(".info-imc");
+  
+  // operadores lógico - validação
+  let pesoInvalido = (peso <= 0) || (peso >= 1000);
+  let alturaInvalido = (altura <= 0) || (altura >= 3);
+  
+  if(pesoInvalido){
+    imcTd.textContent = "Peso Inválido";
+
+    //alterantiva - mudando a cor da linha "tr" pelo js
+    // paciente.style.color = "lightcoral";
+
+    //add classe css
+    paciente.classList.add("paciente-invalido");
+  }
+  else{
+    if(alturaInvalido){
+      imcTd.textContent = "Altura Inválida";
+
+      //add classe css
+      paciente.classList.add("paciente-invalido");
+    }
+    else{
+      let imc = peso / (altura * altura);
+      imcTd.textContent = imc.toFixed(2);
+    }
+  }
+  
+}
+
+
+
--- js/form.js
+++ js/form.js
@@ -1,46 +1,3 @@
-// retorna h1 
-let titulo = document.querySelector('.titulo');
-titulo.textContent = "Aparecida Nutricionista";
-
-//buscando dados do primeiro paciente
-let pacientes = document.querySelectorAll(".paciente");
-
-for (let i = 0; i < pacientes.length; i++) {
-  const paciente = pacientes[i];
-
-  let peso = paciente.querySelector(".info-peso").textContent;
-  let altura = paciente.querySelector(".info-altura").textContent;
-  let imcTd = paciente.querySelector(".info-imc");
-  
-  // operadores lógico - validação
-  let pesoInvalido = (peso <= 0) || (peso >= 1000);
-  let alturaInvalido = (altura <= 0) || (altura >= 3);
-  
-  if(pesoInvalido){
-    imcTd.textContent = "Peso Inválido";
-
-    //alterantiva - mudando a cor da linha "tr" pelo js
-    // paciente.style.color = "lightcoral";
-
-    //add classe css
-    paciente.classList.add("paciente-invalido");
-  }
-  else{
-    if(alturaInvalido){
-      imcTd.textContent = "Altura Inválida";
-
-      //add classe css
-      paciente.classList.add("paciente-invalido");
-    }
-    else{
-      let imc = peso / (altura * altura);
-      imcTd.textContent = imc.toFixed(2);
-    }
-  }
-  
-}
-
-
 //eventos do browser - evento click
 let btnAdicionaPaciente = document.querySelector("#adicionar-paciente");
 
@@ -83,6 +40,3 @@ btnAdicionaPaciente.addEventListener("click", function(event){
   let tabela = document.querySelector("#tabela-pacientes");
   tabela.appendChild(pacienteTr);
 });
-
-
-
