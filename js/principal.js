f59526aca47898ad3f1bb5713453f9f52415289e
sarival <sarival@sarival.com.br>
2020-05-25 16:17:55 -0300

    Add um tr na tabela após click

--- js/principal.js
+++ js/principal.js
@@ -47,7 +47,41 @@ let btnAdicionaPaciente = document.querySelector("#adicionar-paciente");
 btnAdicionaPaciente.addEventListener("click", function(event){
   //evita o reload
   event.preventDefault();
-  console.log("eu fui clicado!");
+  
+  //pegar os dados do form
+  let form = document.querySelector("#form-adiciona");
+
+  // buscando pelo atributo "name"
+  let nome = form.nome.value;
+  let peso = form.peso.value;
+  let altura = form.altura.value;
+  let gordura = form.gordura.value;
+
+  // criando elemntos do html a partir do js
+  let pacienteTr = document.createElement("tr");
+
+  let pesoTd = document.createElement("td");
+  let nomeTd = document.createElement("td");
+  let alturaTd = document.createElement("td");
+  let gorduraTd = document.createElement("td");
+  let imcTd = document.createElement("td");
+
+  // passando os dados para as tds
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+
+  // colocando as tds na tr - appendChild
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+  pacienteTr.appendChild(imcTd);
+
+  // colocando a tr na tabela - DOM
+  let tabela = document.querySelector("#tabela-pacientes");
+  tabela.appendChild(pacienteTr);
 });
 
 
