5be7043f1484e7640d91f474700a028f3c0c1f5b
sarival <sarival@sarival.com.br>
2020-05-26 18:58:33 -0300

    refact

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,7 +1,6 @@
 // colocando o escutador no pai - Delegando eventos (Event Bubbling)
 let table = document.querySelector("table");
 
-
 table.addEventListener("dblclick", function(event){
   event.target.parentNode.remove();
 })
