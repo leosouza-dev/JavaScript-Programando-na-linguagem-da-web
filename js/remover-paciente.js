0fc78ee893b94731303ca0c13b779241fe4dcdd0
sarival <sarival@sarival.com.br>
2020-05-26 18:58:01 -0300

    refatorando o remove paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,10 +3,5 @@ let table = document.querySelector("table");
 
 
 table.addEventListener("dblclick", function(event){
-  // 1 - decobrir quem foi clicado (event.targer - quem foi clicado)
-  let alvo = event.target;
-  // obs - event.target é o td, queremos o tr
-  // 2 - para pegar o tr - usamos o parentNode
-  let paiDoAlvo = event.target.parentNode;
-  paiDoAlvo.remove();
+  event.target.parentNode.remove();
 })
