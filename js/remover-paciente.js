b93d7e7fc06c3a1234fe61ce19d05c478c92ccef
sarival <sarival@sarival.com.br>
2020-05-26 19:46:00 -0300

    animação de remoção de pacinete

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -2,5 +2,10 @@
 let table = document.querySelector("table");
 
 table.addEventListener("dblclick", function(event){
-  event.target.parentNode.remove();
+  event.target.parentNode.classList.add("fadeOut");
+
+  //aguarda os 600ms da animação
+  setTimeout(function(){
+    event.target.parentNode.remove();
+  }, 600);
 })
