37d7c978d1ef435e7c189cea29b0befddfc39e91
sarival <sarival@sarival.com.br>
2020-05-26 18:55:52 -0300

    removendo pacientes - Event Bubbling

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,9 +1,12 @@
-// let pacientes = document.querySelectorAll(".paciente");
-//pacientes já foi declarado em outro arquivo
+// colocando o escutador no pai - Delegando eventos (Event Bubbling)
+let table = document.querySelector("table");
 
-pacientes.forEach(paciente => {
-  paciente.addEventListener("dblclick", function(){
-    this.remove(); // remove a quem o evento está atrelado
-  })
-  
-});
+
+table.addEventListener("dblclick", function(event){
+  // 1 - decobrir quem foi clicado (event.targer - quem foi clicado)
+  let alvo = event.target;
+  // obs - event.target é o td, queremos o tr
+  // 2 - para pegar o tr - usamos o parentNode
+  let paiDoAlvo = event.target.parentNode;
+  paiDoAlvo.remove();
+})
