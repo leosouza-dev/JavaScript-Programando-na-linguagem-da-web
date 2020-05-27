e7401483cce7eb67fb89e0043abbb6072c0e2cca
sarival <sarival@sarival.com.br>
2020-05-26 21:06:43 -0300

    melhorando - se não digitar nada

--- js/filtra.js
+++ js/filtra.js
@@ -1,15 +1,21 @@
 let campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function(){
-  //this.value
-  pacientes.forEach(paciente => {
-    let nome = paciente.querySelector(".info-nome").textContent;
-    
-    if(nome != this.value){
-      paciente.classList.add("invisivel");
-    }else{
+  
+  if(this.value.length > 0){
+    pacientes.forEach(paciente => {
+      let nome = paciente.querySelector(".info-nome").textContent;
+      
+      if(nome != this.value){
+        paciente.classList.add("invisivel");
+      }else{
+        paciente.classList.remove("invisivel");
+      }
+    });
+  }else{
+    pacientes.forEach(paciente => {
       paciente.classList.remove("invisivel");
-    }
-  });
+    });
+  }
 
 });
